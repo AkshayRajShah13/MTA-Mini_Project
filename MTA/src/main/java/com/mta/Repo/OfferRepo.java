b1cd3ca1069ed6380c0d0666976a50fe6ced3a3c
Ashish <ashishsood1310@gmail.com>
2024-02-11 16:45:12 +0530

    folder name changed

--- MTA/src/main/java/com/mta/Repo/OfferRepo.java
+++
@@ -1,9 +0,0 @@
-package com.mta.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mta.entities.Offer;
-
-public interface OfferRepo extends JpaRepository<Offer, Integer>{
-
-}
